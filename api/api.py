84780bdbe49bc2775debbced7d5fe3eb02fd0493
captraj <76588598+captraj@users.noreply.github.com>
2023-07-09 22:40:46 +0530

    added necessary imports

--- api/api.py
+++ api/api.py
@@ -1,3 +1,15 @@
-import tensorflow
+import cv2
+import io
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision.models as models
+import torchvision.transforms as transforms
+from keras.models import load_model
+from PIL import Image
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
-print("hello world")
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
