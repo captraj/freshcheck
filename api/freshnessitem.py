efc088e707ad139f10912a9265d99272cea3ceeb
captraj <76588598+captraj@users.noreply.github.com>
2023-07-09 22:26:40 +0530

    made the process function deal with stream

--- api/freshnessitem.py
+++ api/freshnessitem.py
@@ -1,7 +1,9 @@
 import cv2
+import io
 import numpy as np
 from keras.models import load_model
 from PIL import Image
+from io import BytesIO
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
@@ -11,20 +13,33 @@ CORS(app)  # Enable CORS for all routes
 # Classify fresh/rotten
 def print_fresh(res):
     threshold_fresh = 0.10  # set according to standards
-    threshold_medium = 0.35  # set according to standards
+    threshold_medium = 0.50  # set according to standards
     if res < threshold_fresh:
-        print("The item is FRESH!")
+        return "The item is FRESH!"
     elif threshold_fresh < res < threshold_medium:
-        print("The item is MEDIUM FRESH")
+        return "The item is MEDIUM FRESH"
     else:
-        print("The item is NOT FRESH")
+        return "The item is NOT FRESH"
 
 
-def pre_proc_img(image_path):
+def pre_proc_img(image_data):
+    # Convert the JpegImageFile object to bytes
+    byte_stream = io.BytesIO()
+    image_data = image_data.convert('RGB')  # Convert to RGB
+    image_data.save(byte_stream, format='JPEG')
+    image_bytes = byte_stream.getvalue()
+
+    # Convert the image data to a numpy array
+    img_array = np.frombuffer(image_bytes, dtype=np.uint8)
+
     # Read the image using OpenCV
-    img = cv2.imread(image_path)
-    img = cv2.resize(img, (100, 100))
+    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+
+    # Convert BGR to RGB
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    # Resize the image
+    img = cv2.resize(img, (100, 100))
 
     # Preprocess the image
     img = img / 255.0
@@ -34,7 +49,7 @@ def pre_proc_img(image_path):
 
 def evaluate_rotten_vs_fresh(image_path):
     # Load the trained model
-    model = load_model('trained-freshness-model.h5')
+    model = load_model('rottenvsfresh98pval.h5')
 
     # Read and process and predict
     prediction = model.predict(pre_proc_img(image_path))
@@ -50,5 +65,7 @@ def classify():
     image = request.files['image']
     img = Image.open(image)
     is_rotten = evaluate_rotten_vs_fresh(img)
-    return jsonify({'prediction': is_rotten, 'freshness':print_fresh(is_rotten)})
+    return jsonify({'prediction': str(is_rotten), 'freshness':print_fresh(is_rotten)})
 
+if __name__ == '__main__':
+    app.run(host='::', port=5000)
